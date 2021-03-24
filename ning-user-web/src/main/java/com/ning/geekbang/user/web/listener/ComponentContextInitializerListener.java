2876d6988296baa73be5a8e8d49f6c7d84f4d19d
tongning.guo <39340537+guotongning@users.noreply.github.com>
2021-03-24 20:44:19 +0800

    [feature] 完成包重构

--- ning-user-web/src/main/java/com/ning/geekbang/user/web/listener/ComponentContextInitializerListener.java
+++ ning-user-web/src/main/java/com/ning/geekbang/user/web/listener/ComponentContextInitializerListener.java
@@ -1,4 +1,4 @@
-package com.ning.geekbang.ioc.initializer;
+package com.ning.geekbang.user.web.listener;
 
 
 import com.ning.geekbang.ioc.context.ComponentContext;
@@ -6,36 +6,25 @@ import com.ning.geekbang.ioc.context.ComponentContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
-import java.util.logging.Logger;
-
 
 /**
  * {@link ComponentContext} 初始化器
  * ContextLoaderListener
  */
-@WebListener
-public class ApplicationInitializerListener implements ServletContextListener {
-
-    private static final Logger logger = Logger.getLogger(ApplicationInitializerListener.class.getName());
+public class ComponentContextInitializerListener implements ServletContextListener {
 
     private ServletContext servletContext;
 
-    private ComponentContext context;
-
-
-
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        logger.info("DBConnectionInitializerListener contextInitialized start");
         this.servletContext = sce.getServletContext();
-        context = new ComponentContext();
+        ComponentContext context = new ComponentContext();
         context.init(servletContext);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        context.destroy();
+
     }
 
 }
